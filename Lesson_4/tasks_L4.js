0cc9fc701015b9c3dfb92429a91efd84fc70d6cd
romanZRS <izalytunskyy@icloud.com>
2020-04-05 12:28:00 -0700

    Create folders L1, L2, L3, L4

--- Lesson_4/tasks_L4.js
+++ Lesson_4/tasks_L4.js
@@ -460,73 +460,153 @@
 //
 // listOfCars(cars, 'list');
 
-// todo: next tasks!!!
 // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список
 // цих автомобілів.
 // Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
 // Для кожної властивості створити всередені блока автомоблія свій блок
 //
+// function listOfCars(array, id) {
+//     const carBlock = document.getElementById(`${id}`);
+//
+//     for (let i = 0; i < array.length; i++) {
+//
+//         const car = array[i];
+//
+//         const container = document.createElement('div');
+//         container.style.border = '1px solid red';
+//
+//         const model = document.createElement('div');
+//         const year = document.createElement('div');
+//         const power = document.createElement('div');
+//         const color = document.createElement('div');
+//
+//         model.innerText = ` model - ${car.model}; `;
+//         year.innerText = ` year - ${car.year}; `;
+//         power.innerText = ` power - ${car.power}; `;
+//         color.innerText = ` color - ${car.color}; `;
+//
+//         container.appendChild(model);
+//         container.appendChild(year);
+//         container.appendChild(power);
+//         container.appendChild(color);
+//
+//         carBlock.appendChild(container);
+//     }
+//     document.body.appendChild(carBlock);
+//
+// }
+//
+// listOfCars(cars, 'list');
+
 // (на основі минулого ДЗ)
 // **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з
 // відповідними "id" та "user_id",
 // та повертає масив цих з'єднаних об'єктів.
 // Приклад масивів:
-//let usersWithId = [
+//
+// let usersWithId = [
 // {id: 1, name: 'vasya', age: 31, status: false},
 // {id: 2, name: 'petya', age: 30, status: true},
 // {id: 3, name: 'kolya', age: 29, status: true},
 // {id: 4, name: 'olya', age: 28, status: false},
 // ];
-//let citiesWithId = [
+//
+// let citiesWithId = [
 // {user_id: 3, country: 'USA', city: 'Portland'},
 // {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
 // {user_id: 2, country: 'Poland', city: 'Krakow'},
 // {user_id: 4, country: 'USA', city: 'Miami'},
 // ];
-// Частковий приклад реультату:
 //
 //
+// function concatArrays(array1,array2) {
 //
+//     for (const user of array1) {
+//         for (const city of array2) {
+//             if (user.id === city.user_id) {
+//                 user.address = city;
+//             }
+//         }
+//     }
+//     return array1;
+// }
+//
+//
+// let userWithAddress = concatArrays(usersWithId,citiesWithId);
+// console.log(userWithAddress);
+
+
+let rules = [
+    {
+        title: 'Первое правило Бойцовского клуба.',
+        body: 'Никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Второе правило Бойцовского клуба.',
+        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Третье правило Бойцовского клуба.',
+        body: 'В схватке участвуют только двое.'
+    },
+    {
+        title: 'Четвертое правило Бойцовского клуба.',
+        body: 'Не более одного поединка за один раз.'
+    },
+    {
+        title: 'Пятое правило Бойцовского клуба.',
+        body: 'Бойцы сражаются без обуви и голые по пояс.'
+    },
+    {
+        title: 'Шестое правило Бойцовского клуба.',
+        body: 'Поединок продолжается столько, сколько потребуется.'
+    },
+    {
+        title: 'Седьмое правило Бойцовского клуба.',
+        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+    },
+    {
+        title: 'Восьмое и последнее правило Бойцовского клуба.',
+        body: 'Новичок обязан принять бой.'
+    },
+
+];
+
+
+function createElemDom(array) {
+    const wrap = document.createElement('div');
+    wrap.className = 'wrap';
+
+    for (let i = 0; i < array.length; i++) {
+        const rule = array[i];
+
+        const div = document.createElement('div');
+        div.className = `rules rule${i}`;
+
+        const h2 = document.createElement('h2');
+        const p = document.createElement('p');
+
+        h2.innerText = rule.title;
+        p.innerText = rule.body;
+
+        div.appendChild(h2);
+        div.appendChild(p);
+        wrap.appendChild(div);
+    }
+
+    document.body.appendChild(wrap);
+
+}
+
+createElemDom(rules);
+
+
 // ***- беремо завдання з правилами з укроку №3 :
 // Та робимо це функцією.При цьому правила отримувати через аргумент.
 // "Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило
 // в окремому блоці.
 // При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
 // Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
-// //todo add rules
+
 //
-// 			let rules = [
-// 				{
-// 					title: 'Первое правило Бойцовского клуба.',
-// 					body: 'Никому не рассказывать о Бойцовском клубе.'
-// 				},
-// 				{
-// 					title: 'Второе правило Бойцовского клуба.',
-// 					body: 'Никогда никому не рассказывать о Бойцовском клубе.'
-// 				},
-// 				{
-// 					title: 'Третье правило Бойцовского клуба.',
-// 					body: 'В схватке участвуют только двое.'
-// 				},
-// 				{
-// 					title: 'Четвертое правило Бойцовского клуба.',
-// 					body: 'Не более одного поединка за один раз.'
-// 				},
-// 				{
-// 					title: 'Пятое правило Бойцовского клуба.',
-// 					body: 'Бойцы сражаются без обуви и голые по пояс.'
-// 				},
-// 				{
-// 					title: 'Шестое правило Бойцовского клуба.',
-// 					body: 'Поединок продолжается столько, сколько потребуется.'
-// 				},
-// 				{
-// 					title: 'Седьмое правило Бойцовского клуба.',
-// 					body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
-// 				},
-// 				{
-// 					title: 'Восьмое и последнее правило Бойцовского клуба.',
-// 					body: 'Новичок обязан принять бой.'
-// 				},
-//
-// 			];
+			
